9e5716e64e6e576003c609a01450fdd175217eca
raffiabyann <raffizizou4@gmail.com>
2025-11-19 10:23:45 +0700

    Exception Thrown

---
+++ src/week11/raffi/id/umn/ac/ExceptionThrown.java
@@ -0,0 +1,32 @@
+package week11.raffi.id.umn.ac;
+
+public class ExceptionThrown {
+
+    // Melempar Exception(ArithmeticException)
+    static int divideByZero(int a, int b) {
+        int i = a / b;
+        return i;
+    }
+
+    static int computeDivision(int a, int b) {
+        int res = 0;
+        try {
+            res = divideByZero(a, b);
+        }
+        // tidak sesuai dengan ArithmeticException
+        catch (NumberFormatException ex) {
+            System.out.println("NumberFormatException is occured");
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+        try {
+            int i = computeDivision(a, b);
+        } catch (ArithmeticException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
